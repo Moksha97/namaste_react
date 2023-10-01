0994abec23d95a3a742f2e2e2ed5a1d0ad354217
Mokshagna7 <mokshagna666@gmail.com>
2023-10-01 18:21:47 -0500

    fix-api

--- food_ordering_app/src/components/Body.js
+++ food_ordering_app/src/components/Body.js
@@ -25,8 +25,8 @@ const Body = () => {
         const json = await data.json();
         console.log(json);
         // optional chaining is putting ? ? ?
-        setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
 
     // conditional rendering
